d329aa198264ace4d88ff900483e6b03e8d87c70
Leo <snzleo@gmail.com>
2022-11-13 10:31:31 +0800

    fixed adding study record

--- study-management/classes/subject.php
+++ study-management/classes/subject.php
@@ -11,28 +11,27 @@ class Subject extends Database{
     }
   }
 
-  public function insertSubject($subject_id,$subject_name){
-    $sql = "INSERT INTO `record` (`subject_id`,`subject_name`) VALUES('$subject_id','$subject_name')";
-
-    if($this->conn->query($sql)){
-        header("location: ../views/study-record.php");
+  function insertSingleSubject($subject_id,$user_id) {
+    
+    $sql = "INSERT INTO `record` (`subject_id`,`user_id`) VALUES ($subject_id,$user_id)";
+    if($result = $this->conn->query($sql)){
+      echo "<script>alert('Subject Inserted') </script>";
+      header("location: ../views/subjects.php");
+      return $result;
     } else {
-        die("Error inserting the data!" .$this->conn->error);
+      die ("Something went wrong".$this->conn->error);
     }
+    
   }
 
-  function getUser() {
-    $sql = "SELECT user_id from users LIMIT 1";
-    $result = $this -> conn -> query($sql);
-    return $result;
-  }
 
   function getStudyRecords($user_id) {
     $sql = "SELECT s.subject_id, s.subject_name, r.clock_in, r.clock_out, total, r.note FROM record r INNER JOIN subjects s ON r.subject_id = s.subject_id INNER JOIN users u ON r.user_id = u.user_id WHERE u.user_id = $user_id";
   
-    if($result = $this->conn->query($sql))
-    {
-      return $result;
+    if($result = $this->conn->query($sql)) {
+      if ($result -> num_rows > 0) {
+       return $result;
+      }
     }
     else {
       echo $this->error;
